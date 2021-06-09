f1dc970469f5fdc5eb7328c4e42780e02d54a182
StevenCreates <steven@stevencreates.tech>
2021-06-09 15:28:10 -0600

    changed npm packages

--- src/components/InfoPage.js
+++ src/components/InfoPage.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
+import Editor from "react-code-live";
 
 const InfoDescription = styled.div`
   margin: 100px;
@@ -9,24 +9,29 @@ const InfoDescription = styled.div`
 const LiveEditorContainer = styled.div`
     max-width: 900px,
     min-width: 600px
-`
+`;
 
 export const InfoPage = ({ title, description, initialCode }) => {
   return (
     <div>
-        <LiveEditorContainer>
-      <LiveProvider  code="<strong>Hello World!</strong>">
-        <LiveEditor  style={{
-          background: `#343434`,
-          width: `85vw`,
-          minHeight: `25vh`,
-          maxHeight: `50vh`
-      }}/>
-        <LiveError />
-        <LivePreview />
-      </LiveProvider>
-        </LiveEditorContainer>
+      <Editor
+        storageIdentifier="preview-container"
+        initialJs={initialReactCode}
+        showControls={true}
+      />
+      <LiveEditorContainer data-id="preview-container"></LiveEditorContainer>
       <InfoDescription>Testing</InfoDescription>
     </div>
   );
 };
+
+const initialReactCode = `const WithStyled = () => {
+  const [value, setValue] = useState('initialValue')
+  return (
+    <>
+      <h5>{value}</h5>
+      <input type="text" onChange={e => setValue(e.target.value)} value={value} />
+    </>
+  )
+  }
+`;
