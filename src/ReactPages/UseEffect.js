ed9a1647e6367ef92683ce6313fc7f0556b00424
StevenCreates <steven@stevencreates.tech>
2021-06-14 08:21:50 -0600

    added order to infocontainer

--- src/ReactPages/UseEffect.js
+++ src/ReactPages/UseEffect.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import styled from "styled-components";
+import { InformationContainer } from "../styledComponents/InformationContainer";
+
+const Grid = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: flex-start;
+  align-content: flex-start;
+  align-items: flex-start;
+`;
 
 export const UseEffect = () => {
-    return (
-        <div>
-            Hello
-        </div>
-    )
-}
+  return (
+  <Grid>
+      Hello
+  </Grid>
+  )
+};
--- src/styledComponents/InformationContainer.js
+++ src/styledComponents/InformationContainer.js
@@ -5,21 +5,24 @@ export const InformationContainer = ({
   height,
   titleBackground,
   title,
-  children
+  children,
+  order,
 }) => {
-
   const InfoContainer = styled.div`
-  background: #343434;
-  border-radius: 2px;
-  color: white;
-  height: ${ height ? height : '15vh'};
-  width: ${ width ? width : '49.5%'};
-  max-width: ${ width ? width : '49.5%'};
-  -webkit-box-shadow: 0px 10px 13px -7px #000000,
-    5px 5px 15px 5px rgba(255, 255, 255, 0);
-  box-shadow: 0px 10px 13px -7px #000000,
-    5px 5px 15px 5px rgba(255, 255, 255, 0);
-`;
+    order: ${order ? order : "0"};
+    flex: 0 1 auto;
+    align-self: auto;
+    background: #343434;
+    border-radius: 2px;
+    color: white;
+    height: ${height ? height : "15vh"};
+    width: ${width ? width : "49.5%"};
+    max-width: ${width ? width : "49.5%"};
+    -webkit-box-shadow: 0px 10px 13px -7px #000000,
+      5px 5px 15px 5px rgba(255, 255, 255, 0);
+    box-shadow: 0px 10px 13px -7px #000000,
+      5px 5px 15px 5px rgba(255, 255, 255, 0);
+  `;
 
   const Title = styled.div`
     position: absolute;
@@ -28,7 +31,7 @@ export const InformationContainer = ({
     color: black;
     font-size: 18px;
     text-align: left;
-    background: ${ titleBackground ? titleBackground : '#ffffff'};
+    background: ${titleBackground ? titleBackground : "#ffffff"};
     display: inline-block;
     -webkit-box-shadow: 0px 10px 13px -7px #000000,
       5px 5px 15px 5px rgba(255, 255, 255, 0);
